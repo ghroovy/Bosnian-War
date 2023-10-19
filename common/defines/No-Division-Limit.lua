33719a1ee7bb2bbe50f886089fa7fe41c117c715
12345 <128200967+vodavvodda@users.noreply.github.com>
2023-10-19 19:56:18 +0200

    units

---
+++ common/defines/No-Division-Limit.lua
@@ -0,0 +1,13 @@
+-- Infinite Corps and Field Marshal Limits.
+
+-- how many armies a field marshal can hold. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 7;	
+
+-- how many divisions a field marshal can hold. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 50;
+
+-- how many armies a corp commander can hold. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1;
+
+-- how many divisions a corp commander can hold. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 50;
